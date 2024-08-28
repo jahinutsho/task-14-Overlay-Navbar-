bc3fc140a2e9027f25310f0d9552220f3bec0689
Jahin Utsho <jutsho2@gmail.com>
2024-08-28 22:10:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function closeMenu2(){
+    document.getElementById('navbar2').style.height = "0%";
+}
+function openMenu2(){
+    document.getElementById('navbar2').style.height = "100%";
+}
+function closeMenu(){
+    document.getElementById('navbar').style.width = "0%";
+}
+function openMenu(){
+    document.getElementById('navbar').style.width = "100%";
+}
